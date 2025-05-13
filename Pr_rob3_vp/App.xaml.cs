93621d9a3ab876166e8561c2f280bc1b0556119c
Anastasia9797 <n6609400@gmail.com>
2025-05-13 19:46:59 +0300

    Добавьте файлы проекта.

---
+++ Pr_rob3_vp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Pr_rob3_vp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
